761e38ff5ce2f13c80288745c914efaad1999389
GinSanaduki <48995370+GinSanaduki@users.noreply.github.com>
2020-03-29 01:44:27 +0900

    Delete Call_Processing.sh

--- ShellScripts/Call_Processing.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Call_Processing.sh
-# sh ./ShellScripts/Call_Processing.sh
-
-ps -aux | frep chromedriver | grep -v grep | awk 'END{if(NR > 255){exit 255;} exit NR;}'
-Retcode=$?
-ps -aux | fgrep chromedriver | awk '{print > /dev/stderr;}'
-exit $Retcode
-
